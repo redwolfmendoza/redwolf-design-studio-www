aca9e690efa3f9759a4bde0094729d3540565984
James Mendoza <redwolfmendoza@gmail.com>
2018-01-31 16:12:05 -0800

    Added files to Gitignore

---
+++ local-config-sample.php
@@ -0,0 +1,11 @@
+/** The name of the database for WordPress */
+define('DB_NAME', 'db');
+
+/** MySQL database username */
+define('DB_USER', 'username');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'password');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
--- wp-config.php
+++ wp-config.php
@@ -18,18 +18,8 @@
  * @package WordPress
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'rds');
-
-/** MySQL database username */
-define('DB_USER', 'root');
-
-/** MySQL database password */
-define('DB_PASSWORD', 'mysql');
-
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
+/** MySQL settings **/
+require_once(ABSPATH . 'local-config.php');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
