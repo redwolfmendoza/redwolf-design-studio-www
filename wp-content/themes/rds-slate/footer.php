79815bedb432fef429a2837ce1312d0122f950aa
James Mendoza <redwolfmendoza@gmail.com>
2018-01-31 17:04:05 -0800

    Divided index.php into sections
    - Created individual header, footer, content and sidebar PHP templates

---
+++ wp-content/themes/rds-slate/footer.php
@@ -0,0 +1,20 @@
+</div><!-- /.container -->
+
+    <footer class="blog-footer">
+      <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
+
+
+    <!--jQuery-->
+    <script src="<?php echo get_bloginfo('template_directory'); ?>/js/jquery-3.3.1.min.js"></script>
+      
+    <!--Boostrap JS-->
+    <script src="<?php echo get_bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
+      
+    <?php wp_footer(); ?>
+
+  </body>
+</html>
