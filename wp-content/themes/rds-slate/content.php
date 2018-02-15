f66ecd19e3b9dae5f5e3243f9dddd11f07073043
James Mendoza <redwolfmendoza@gmail.com>
2018-02-14 17:28:40 -0800

    Updated theme profile

--- wp-content/themes/rds-slate/content.php
+++ wp-content/themes/rds-slate/content.php
@@ -1,11 +1,13 @@
 <section class="homepage-section">
     <div class="logo-container">
         <div class="logo">
-            <img class="img-responsive" src="https://www.redwolfdesignstudio.com/wp-content/uploads/2017/07/rds2-logo-640x640.png" width="320" height="320">
+            <a href="/">
+                <img class="img-responsive" src="wp-content/themes/rds-slate/img/rds-640x640.png" width="320" height="320">
+            </a>
         </div>
     </div>
     <div class="intro-container">
         <h1 class="rds-branding">Redwolf&nbsp;Design&nbsp;Studio</h1>
-        <h4 class="rds-slogan">Professional web development and web design services. San Fernando Valley, CA.</h4>
+        <h4 class="rds-slogan">Professional web development and design services since 2008. San Fernando Valley, CA.</h4>
     </div>
 </section>
